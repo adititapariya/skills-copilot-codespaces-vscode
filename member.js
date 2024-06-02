1553ebd76345a27562224c80363ce43fd522ee66
Aditi Tapariya <tapariyaaditi23@gmail.com>
2024-06-02 13:27:24 +0530

    Step 3 completed

---
+++ member.js
@@ -0,0 +1,17 @@
+function skillsMember() {
+  var skills = [
+    {
+      name: "HTML",
+      level: 9
+    },
+    {
+      name: "CSS",
+      level: 8
+    },
+    {
+      name: "JavaScript",
+      level: 7
+    }
+  ];
+  return skills;
+}
